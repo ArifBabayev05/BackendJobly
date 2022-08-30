2514d0c55b07898a1123860bdac258c38d03fd8b
imac <imac@imacs-MacBook-Pro.local>
2022-08-30 16:29:48 +0400

    Auth Service

---
+++ Business/Abstract/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Jwt;
+
+namespace Business.Abstract
+{
+    public interface IAuthService
+    {
+        IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password);
+        IDataResult<User> Login(UserForLoginDto userForLoginDto);
+        IResult UserExists(string email);
+        IDataResult<AccessToken> CreateAccessToken(User user);
+    }
+}
+
