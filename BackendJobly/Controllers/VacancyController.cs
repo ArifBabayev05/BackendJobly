bd30e9f5bdc23b576585659f1a4f4de262688221
imac <imac@imacs-MacBook-Pro.local>
2022-08-31 12:42:58 +0400

    Finish Vacancy Controller

--- BackendJobly/Controllers/VacancyController.cs
+++ BackendJobly/Controllers/VacancyController.cs
@@ -1,15 +1,94 @@
 ﻿using System;
+using Business.Abstract;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BackendJobly.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class VacancyController
+    public class VacanciesController : ControllerBase
     {
-        public VacancyController()
+        private IVacancyService _vacancyService;
+
+        public VacanciesController(IVacancyService vacancyService)
         {
+            _vacancyService = vacancyService;
         }
+
+        [HttpGet("getall")]
+        public IActionResult GetList()
+        {
+            var result = _vacancyService.GetList();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+
+        }
+
+        [HttpGet("getlistbycategory")]
+        public IActionResult GetListByCategory(int categoryId)
+        {
+            var result = _vacancyService.GetListByCategory(categoryId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult Get(int id)
+        {
+            var result = _vacancyService.Get(id);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Vacancy vacancy)
+        {
+            var result = _vacancyService.Add(vacancy);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Vacancy vacancy)
+        {
+            var result = _vacancyService.Update(vacancy);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(Vacancy vacancy)
+        {
+            var result = _vacancyService.Delele(vacancy);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+
     }
 }
 
