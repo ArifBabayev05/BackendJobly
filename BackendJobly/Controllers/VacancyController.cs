8d66c48ac7e0c6f71ddb42eec41725a0828f3d26
imac <imac@imacs-MacBook-Pro.local>
2022-08-31 12:14:55 +0400

    Create Vacancy Controller

--- BackendJobly/Controllers/VacancyController.cs
+++ BackendJobly/Controllers/VacancyController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
 namespace BackendJobly.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class VacancyController
     {
         public VacancyController()
