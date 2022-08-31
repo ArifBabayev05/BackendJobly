d11595bc1bcffff2b288187f39f9190365eb5d60
imac <imac@imacs-MacBook-Pro.local>
2022-08-31 12:42:08 +0400

    Finish Company Controller

--- BackendJobly/Controllers/CompanyController.cs
+++ BackendJobly/Controllers/CompanyController.cs
@@ -1,15 +1,65 @@
 ﻿using System;
+using Business.Abstract;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BackendJobly.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CompanyController
+    public class CompanyController : ControllerBase
     {
+        private ICompanyService _companyService;
 
-        public CompanyController()
+        public CompanyController(ICompanyService companyService)
         {
+            _companyService = companyService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetList()
+        {
+            var result = _companyService.GetList();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Company company)
+        {
+            var result = _companyService.Add(company);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Company company)
+        {
+            var result = _companyService.Update(company);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(Company company)
+        {
+            var result = _companyService.Delele(company);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
         }
     }
 }
