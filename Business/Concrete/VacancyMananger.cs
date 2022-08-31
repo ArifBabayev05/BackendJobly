176e2ff56805245fe556ea76a5a58245a1160636
imac <imac@imacs-MacBook-Pro.local>
2022-08-31 12:23:48 +0400

    Finish Vacancy Manager

--- Business/Concrete/VacancyMananger.cs
+++ Business/Concrete/VacancyMananger.cs
@@ -1,10 +1,54 @@
 ﻿using System;
+using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using System.Collections.Generic;
+
 namespace Business.Concrete
 {
-    public class VacancyMananger
+    public class VacancyManager : IVacancyService
     {
-        public VacancyMananger()
+        private IVacancyDal _vacancyDal;
+
+        public VacancyManager(IVacancyDal vacancyDal)
         {
+            _vacancyDal = vacancyDal;
+        }
+
+        public IDataResult<Vacancy> Get(int id)
+        {
+            return new SuccessDataResult<Vacancy>(_vacancyDal.Get(p => p.Id == id));
+        }
+
+        public IDataResult<List<Vacancy>> GetList()
+        {
+            return new SuccessDataResult<List<Vacancy>>(_vacancyDal.GetList().ToList());
+        }
+
+        public IDataResult<List<Vacancy>> GetListByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Vacancy>>(_vacancyDal.GetList(p => p.CategoryId == categoryId).ToList());
+        }
+
+        public IResult Add(Vacancy vacancy)
+        {
+            _vacancyDal.Add(vacancy);
+            return new SuccessResult(Messages.Add);
+        }
+
+        public IResult Delele(Vacancy vacancy)
+        {
+            _vacancyDal.Delete(vacancy);
+            return new SuccessResult(Messages.Delete);
+
+        }
+
+        public IResult Update(Vacancy vacancy)
+        {
+            _vacancyDal.Update(vacancy);
+            return new SuccessResult(Messages.Update);
+
         }
     }
 }
