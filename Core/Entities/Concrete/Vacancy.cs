25d1988074639b732a77137cd397f698b6f34a07
Arif Babayev <arifrb@code.edu.az>
2022-09-25 03:32:03 +0400

    Fix some warnings

--- Core/Entities/Concrete/Vacancy.cs
+++ Core/Entities/Concrete/Vacancy.cs
@@ -14,17 +14,17 @@ namespace Entities.Concrete
             Users = new List<User>();
         }
         public string Name { get; set; }
-        public City? City { get; set; }
-        public int? CityId { get; set; }
+        public City City { get; set; }
+        public int CityId { get; set; }
         public DateTime? Deadline { get; set; }
         public string TypeOfwork { get; set; }
         public string VəzifəÖhdəlikləri { get; set; }
         public string Tələblər { get; set; }
         public int Salary { get; set; }
-        public Category? Category { get; set; }
-        public int? CategoryId { get; set; }
-        public Company? Company { get; set; }
-        public int? CompanyId { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+        public Company Company { get; set; }
+        public int CompanyId { get; set; }
         //public int UserId { get; set; }
         List<User> Users { get; set; }
     }
