dd728e7b3eab8414de89a23e24afdf882fa22aad
Arif Babayev <arifrb@code.edu.az>
2022-09-26 22:53:41 +0400

    Finish image service

--- Business/Abstract/IImageService.cs
+++ Business/Abstract/IImageService.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using Core.Utilities.Results;
+using Entities.Models;
+using System.Collections.Generic;
+
 namespace Business.Abstract
 {
     public interface IImageService
     {
+        IDataResult<Company> Get(int id);
+
+        IDataResult<List<Company>> GetList();
+
+        IResult Add(Company company);
+
+        IResult Delele(int id);
+
+        IResult Update(Company company, int id);
     }
 }
 
